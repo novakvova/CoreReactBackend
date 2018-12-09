c7259c5f15c427c5912bb2e48ad249c01d33cfcd
novakvova <novakvova@gmail.com>
2018-12-09 12:36:50 +0200

    Begin create aps.net core

---
+++ BackendCore/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendCore.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(SignupBindingModel model)
+        {
+            bool isValid = true;
+            ErrorsSignupBindingModel errors = new ErrorsSignupBindingModel();
+            if(string.IsNullOrEmpty(model.UserName))
+            {
+                errors.username = "This field is required";
+                isValid = false;
+            }
+            if (!isValid)
+                return BadRequest(new { errors, isValid });
+
+            return Ok(new { success=true });
+            //return View();
+        }
+    }
+}
---
+++ BackendCore/ViewModels/SignupBindingModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendCore.ViewModels
+{
+    public class SignupBindingModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Timezone { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirmation { get; set; }
+
+    }
+    public class ErrorsSignupBindingModel
+    {
+        public string email { get; set; }
+        public string username { get; set; }
+        public string timezone { get; set; }
+        public string password { get; set; }
+        public string passwordConfirmation { get; set; }
+    }
+}
